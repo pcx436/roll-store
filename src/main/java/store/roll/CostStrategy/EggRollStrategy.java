4f4140aff71c19a0baff845793690b2df7dafbc2
kyle <kyleastewart1@gmail.com>
2020-10-13 21:38:24 -0600

    cost strategy
    setting costs for the strategy pattern

--- src/main/java/store/roll/CostStrategy/EggRollStrategy.java
+++ src/main/java/store/roll/CostStrategy/EggRollStrategy.java
@@ -1,4 +1,19 @@
 package store.roll.CostStrategy;
 
 public class EggRollStrategy implements CostStrategy {
+
+    @Override
+    public double getFillingCost() {
+        return 0.70;
+    }
+
+    @Override
+    public double getSauceCost() {
+        return 0.40;
+    }
+
+    @Override
+    public double getToppingCost() {
+        return 3.50;
+    }
 }
--- src/main/java/store/roll/CostStrategy/PastryRollStrategy.java
+++ src/main/java/store/roll/CostStrategy/PastryRollStrategy.java
@@ -1,4 +1,18 @@
 package store.roll.CostStrategy;
 
 public class PastryRollStrategy implements CostStrategy {
+    @Override
+    public double getFillingCost() {
+        return 1.51;
+    }
+
+    @Override
+    public double getSauceCost() {
+        return 7.93;
+    }
+
+    @Override
+    public double getToppingCost() {
+        return 81.42;
+    }
 }
