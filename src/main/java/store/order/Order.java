f75157f15fb1bc31d366ae05c84607a4cdab421d
Jacob Malcy <pcx436@yahoo.com>
2020-10-14 10:11:21 -0600

    Creating rollCount array in Order class

--- src/main/java/store/order/Order.java
+++ src/main/java/store/order/Order.java
@@ -7,9 +7,10 @@ import java.util.List;
 
 public abstract class Order {
     protected List<Roll> rolls;
+    protected int[] rollCount = {0, 0, 0, 0, 0};
 
     public Order(List<Roll> rolls) {
-        this.rolls = rolls;
+        this.setRolls(rolls);
     }
 
 
@@ -19,6 +20,15 @@ public abstract class Order {
 
     public void setRolls(List<Roll> rolls) {
         this.rolls = rolls;
+
+        // wipe current counts
+        for (int i = 0; i < 5; i++)
+            rollCount[i] = 0;
+
+        // create array that has count of rolls by type
+        for (Roll r: this.rolls) {
+            rollCount[r.getType() - 1]++;
+        }
     }
 
     public Boolean placeOrder(Inventory invent) {
