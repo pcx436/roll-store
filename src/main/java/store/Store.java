69a079439fbc85bb97d5f1850442377ac979ff7c
Jacob Malcy <pcx436@yahoo.com>
2020-10-14 13:05:32 -0600

    WIP: Added daily print system

--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -1,5 +1,9 @@
 package store;
 
+import store.order.Order;
+import store.order.OrderFactory;
+import java.util.List;
+
 public class Store {
 
     private static void printInventory(Inventory inventory) {
@@ -10,6 +14,26 @@ public class Store {
     }
 
     public static void main(String[] args){
+        OrderFactory orderFactory = new OrderFactory();
+        Inventory inventory = new Inventory();
+        List<Order> orderList;
 
+        for(int day = 1; day <= 30; day++) {
+            orderList = orderFactory.createOrders();
+
+            System.out.println("It is day #" + day);
+            printInventory(inventory);
+
+            for (Order order: orderList) {
+                double orderTotal = order.placeOrder(inventory);
+
+                if (orderTotal < 0.0) {  // order failed
+                    // TODO:
+                }
+            }
+
+            printInventory(inventory);
+            inventory.refillRoll();
+        }
     }
 }
