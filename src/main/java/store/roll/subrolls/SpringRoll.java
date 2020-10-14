2af86fc90ee16a1b709e8d9a439485cfa42bf79f
Jacob Malcy <pcx436@yahoo.com>
2020-10-13 20:36:24 -0600

    Filling out Roll constructors

--- src/main/java/store/roll/subrolls/SpringRoll.java
+++ src/main/java/store/roll/subrolls/SpringRoll.java
@@ -1,6 +1,12 @@
 package store.roll.subrolls;
 
+import store.roll.CostStrategy.SpringRollStrategy;
 import store.roll.Roll;
 
 public class SpringRoll extends Roll {
+    public SpringRoll() {
+        this.baseCost = 1.99;
+        this.description = "fall roll";
+        this.costStrategy = new SpringRollStrategy();
+    }
 }
