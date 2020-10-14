876b586e6c96c290c840deb6025a3b7cb9249808
Vince <vincentliu362@gmail.com>
2020-10-13 21:14:06 -0600

    declare the roll, made a constructor, add description and cost methods

--- src/main/java/store/roll/RollDecorator/RollDecorator.java
+++ src/main/java/store/roll/RollDecorator/RollDecorator.java
@@ -3,4 +3,17 @@ package store.roll.RollDecorator;
 import store.roll.Roll;
 
 public abstract class RollDecorator extends Roll {
+    protected Roll roll;
+
+    public RollDecorator(Roll roll) {
+        this.roll = roll;
+    }
+
+    public String getDescription(){
+        return this.roll.getDesciption();
+    }
+
+    public double getCost(){
+        return this.roll.getCost();
+    }
 }
