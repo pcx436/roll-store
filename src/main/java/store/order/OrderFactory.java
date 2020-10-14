83fc0dbe0ce05143e7304519addfeaa247813d63
Jacob Malcy <pcx436@yahoo.com>
2020-10-13 22:54:06 -0600

    Committing rollFactory creation

--- src/main/java/store/order/OrderFactory.java
+++ src/main/java/store/order/OrderFactory.java
@@ -1,4 +1,11 @@
 package store.order;
 
+import store.roll.RollFactory;
+
 public class OrderFactory {
+    private final RollFactory rollFactory;
+
+    public OrderFactory() {
+        rollFactory = new RollFactory();
+    }
 }
