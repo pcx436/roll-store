52aa4be9112914f31cc3c47fab6233f521a488b6
Vince <vincentliu362@gmail.com>
2020-10-13 21:15:03 -0600

    create constructor, made method description and cost

--- src/main/java/store/roll/RollDecorator/ExtraTopping.java
+++ src/main/java/store/roll/RollDecorator/ExtraTopping.java
@@ -1,4 +1,18 @@
 package store.roll.RollDecorator;
 
+import store.roll.Roll;
+
 public class ExtraTopping extends RollDecorator {
+    public ExtraTopping(Roll roll){
+        super(roll);
+        System.out.println(", with extra topping");
+    }
+
+    public String getDescription(){
+        return this.roll.getDescription() + ", topping";
+    }
+
+    public double getCost(){
+        return this.roll.getBaseCost() + this.roll.getCostStrategy().getToppingCost();
+    }
 }
