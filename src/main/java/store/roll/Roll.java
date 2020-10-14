77aff59bae1a00c5ed1fe3160721189aba959f9e
Jacob Malcy <pcx436@yahoo.com>
2020-10-13 20:35:44 -0600

    Changed properties to protected

--- src/main/java/store/roll/Roll.java
+++ src/main/java/store/roll/Roll.java
@@ -3,9 +3,10 @@ package store.roll;
 import store.roll.CostStrategy.CostStrategy;
 
 public abstract class Roll {
-    private String description;
-    private float baseCost;
-    private CostStrategy costStrategy;
+    protected String description;
+    protected double baseCost;
+    protected CostStrategy costStrategy;
+
 
     
 }
