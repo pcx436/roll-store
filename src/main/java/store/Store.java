5a5aca4cad80ae2206c5e20b6d644a724c8b3fb7
Jacob Malcy <pcx436@yahoo.com>
2020-10-14 13:05:16 -0600

    adding printInventory

--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -1,6 +1,14 @@
 package store;
 
 public class Store {
+
+    private static void printInventory(Inventory inventory) {
+        System.out.println("The current inventory is as follows:");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("\t" + inventory.typeToString(i) + ": " + inventory.getRollAmount(i));
+        }
+    }
+
     public static void main(String[] args){
 
     }
