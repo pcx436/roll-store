a5000f364ced73eeb40006438f23b123678d21dc
Jacob Malcy <pcx436@yahoo.com>
2020-10-13 20:35:51 -0600

    Add getters and setters

--- src/main/java/store/roll/Roll.java
+++ src/main/java/store/roll/Roll.java
@@ -7,6 +7,27 @@ public abstract class Roll {
     protected double baseCost;
     protected CostStrategy costStrategy;
 
+    public String getDescription() {
+        return description;
+    }
 
-    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getBaseCost() {
+        return baseCost;
+    }
+
+    public void setBaseCost(double baseCost) {
+        this.baseCost = baseCost;
+    }
+
+    public CostStrategy getCostStrategy() {
+        return costStrategy;
+    }
+
+    public void setCostStrategy(CostStrategy costStrategy) {
+        this.costStrategy = costStrategy;
+    }
 }
