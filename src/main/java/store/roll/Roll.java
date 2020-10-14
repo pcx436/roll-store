8c35b0dd5a36003c9e72fc617546851aa5ce91f1
Jacob Malcy <pcx436@yahoo.com>
2020-10-13 20:21:07 -0600

    Adding instance variables

--- src/main/java/store/roll/Roll.java
+++ src/main/java/store/roll/Roll.java
@@ -1,4 +1,11 @@
 package store.roll;
 
+import store.roll.CostStrategy.CostStrategy;
+
 public abstract class Roll {
+    private String description;
+    private float baseCost;
+    private CostStrategy costStrategy;
+
+    
 }
