3c42d7cdff77eba369ea500859d38d280d05a4fb
Jacob Malcy <pcx436@yahoo.com>
2020-10-14 14:33:42 -0600

    Catering type assertion

--- src/test/java/MyUnitTest.java
+++ src/test/java/MyUnitTest.java
@@ -2,13 +2,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import store.Inventory;
+import store.roll.Roll;
+import store.roll.RollFactory;
+
+import java.util.List;
 
 public class MyUnitTest {
     Inventory inventory;
+    RollFactory rollFactory;
 
     @BeforeEach
     public void setup() {
         inventory = new Inventory();
+        rollFactory = new RollFactory();
     }
 
     @Test
@@ -30,5 +36,23 @@ public class MyUnitTest {
     @Test
     public void testTypeToStringJellyRoll() {
         Assertions.assertEquals("jelly roll", inventory.typeToString(5));
+    }
+
+    @Test
+    public void testValidCatering() {
+        List<Roll> rollList = rollFactory.caterOrder();
+
+        // got 15 rolls
+        Assertions.assertEquals(15, rollList.size());
+
+        for (int i = 0; i < 5; i++) {
+            Assertions.assertEquals(rollList.get(0).getType(), rollList.get(i).getType());
+        }
+        for (int i = 5; i < 10; i++) {
+            Assertions.assertEquals(rollList.get(5).getType(), rollList.get(i).getType());
+        }
+        for (int i = 10; i < 15; i++) {
+            Assertions.assertEquals(rollList.get(10).getType(), rollList.get(i).getType());
+        }
     }
 }
